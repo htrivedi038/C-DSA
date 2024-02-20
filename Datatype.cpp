d15cfed9ea66cb48f1c2c50258d1bdbbe58b007e
Himanshu Trivedi <htrivedi038@gmail.com>
2024-02-20 20:59:46 +0530

    Create Datatype.cpp

---
+++ Datatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a=5;
+cout<<a<<endl;
+char b='k';
+cout<<b<<endl;
+bool c=false;
+cout<<c<<endl;
+float d=1.3;
+cout<<d<<endl;
+double e=5.55;
+cout<<e<<endl;
+
+}
