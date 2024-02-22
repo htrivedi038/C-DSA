4a4d7839bdd3433fe75fb34eac910290e6bb7151
Himanshu Trivedi <htrivedi038@gmail.com>
2024-02-22 19:42:35 +0530

    Create bitwise operator.cpp

---
+++ bitwise operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3;
+    int b=5;
+    cout<<"a&b is :"<<(a&b)<<endl;
+    cout<<"a|b is :"<<(a|b)<<endl;
+    cout<<"~a is :"<<(~a)<<endl;
+    cout<<"a^b is :"<<(a^b)<<endl;
+}
