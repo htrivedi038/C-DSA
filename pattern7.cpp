a8319df48cf06ef1b17327b37072332c3e970521
Himanshu Trivedi <htrivedi038@gmail.com>
2024-02-22 19:48:52 +0530

    Create pattern7.cpp

---
+++ pattern7.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n ;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+       
+        while(j<=i)
+        {
+            cout<<i-1+j<<" ";
+            
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    /*2nd method*/
+    while(i<=n)
+    {
+        int j=1;
+        int value=i;
+        while(j<=i)
+        {
+            cout<<value;
+            value=value+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
